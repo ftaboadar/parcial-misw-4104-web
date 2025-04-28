5a1dfe0e2e3efd2f53b43f2c80786497f5ff97d8
ftaboadar <f.taboada@uniandes.edu.co>
2025-04-27 22:11:05 -0500

    login

---
+++ src/app/components/login/login.component.ts
@@ -0,0 +1,38 @@
+import { NgIf } from '@angular/common';
+import { Component } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-login',
+  standalone: true, // importante en Angular 19 si quieres usarlo sin módulo
+  imports: [ReactiveFormsModule, NgIf],
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+export class LoginComponent {
+  loginForm: FormGroup = new FormGroup({
+    userName: new FormControl(""),
+    password: new FormControl(""),
+  });
+
+  showError: boolean = false; 
+
+  constructor(private router: Router) {}
+
+  onSubmit() {
+    const { userName, password } = this.loginForm.value;
+
+    if (userName === 'admin' && password === 'admin') {
+      this.router.navigate(['']);
+      this.showError = false; 
+    } else {
+      this.showError = true;
+    }
+  }
+
+  onCancel() {
+    this.loginForm.reset();
+    this.showError = false; 
+  }
+}
