53e6ea51e6fdf67681febb036d12a4f8302c0631
ftaboadar <f.taboada@uniandes.edu.co>
2025-04-27 23:20:49 -0500

    funcionalidades finales

--- src/app/components/vehicle-list/vehicle-list.component.ts
+++ src/app/components/vehicle-list/vehicle-list.component.ts
@@ -1,11 +1,39 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { CommonModule, NgIf, NgFor, KeyValuePipe } from '@angular/common';
+import { Vehicle } from '../../models/vehicle.model';
 
 @Component({
   selector: 'app-vehicle-list',
-  imports: [],
+  imports: [CommonModule, NgIf, NgFor, KeyValuePipe],
   templateUrl: './vehicle-list.component.html',
   styleUrl: './vehicle-list.component.css'
 })
 export class VehicleListComponent {
+  vehicles: Vehicle[] = [];
+  selectedVehicle: Vehicle | null = null;
+  marcaTotals: { [marca: string]: number } = {};
+
+  private apiUrl = 'https://gist.githubusercontent.com/josejbocanegra/17bb8c76405e43655d551a90800c8a81/raw/d41b4acc3457e51e7533fad6d5e9925ee9676457/202212_MISW4104_Grupo1.json';
+
+  constructor(private http: HttpClient) {}
+
+  ngOnInit() {
+    this.http.get<any[]>(this.apiUrl).subscribe(data => {
+      this.vehicles = data.map(item => new Vehicle(item));
+      this.calculateTotals();
+    });
+  }
+
+  onSelect(vehicle: Vehicle) {
+    this.selectedVehicle = vehicle;
+  }
+
+  private calculateTotals() {
+    this.marcaTotals = {};
+    for (let vehicle of this.vehicles) {
+      this.marcaTotals[vehicle.marca] = (this.marcaTotals[vehicle.marca] || 0) + 1;
+    }
+  }
 
 }
--- src/app/services/vehicle.service.ts
+++ src/app/services/vehicle.service.ts
@@ -1,16 +1,15 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
-import { Vehicle } from '../models/vehicle.model';
+import { Vehicle } from '../models/vehicle.model'; // Asegúrate de que esta ruta esté correcta
 
 @Injectable({
   providedIn: 'root'
 })
 export class VehicleService {
-
   private apiUrl = 'https://gist.githubusercontent.com/josejbocanegra/17bb8c76405e43655d551a90800c8a81/raw/d41b4acc3457e51e7533fad6d5e9925ee9676457/202212_MISW4104_Grupo1.json';
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient) {}
 
   getVehicles(): Observable<Vehicle[]> {
     return this.http.get<Vehicle[]>(this.apiUrl);
